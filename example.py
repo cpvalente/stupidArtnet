f100efd27aa668ba1729f2f8c9c5547c61f64f8b
CV_ <34649812+digitalanimalscollective@users.noreply.github.com>
2018-04-14 23:14:16 +0200

    thread example

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 from lib.StupidArtnet import StupidArtnet
 import time
+import random
 
 # THESE ARE MOST LIKELY THE VALUES YOU WILL BE NEEDING
 target_ip = '192.168.1.10'		# typically in 2.x or 10.x range
@@ -7,13 +8,12 @@ universe = 0 					# see docs
 packet_size = 100				# it is not necessary to send whole universe
 
 # CREATING A STUPID ARTNET OBJECT
-a = StupidArtnet()
-
-# SETUP NEEDS FEW SKIPPABLE ELEMENTS
+# SETUP NEEDS A FEW ELEMENTS
 # TARGET_IP   = DEFAULT 127.0.0.1
 # UNIVERSE    = DEFAULT 0
 # PACKET_SIZE = DEFAULT 512
-a.setup(target_ip, universe, packet_size)
+# FRAME_RATE  = DEFAULT 30
+a = StupidArtnet(target_ip, universe, packet_size)
 
 # MORE ADVANCED CAN BE SET WITH SETTERS IF NEEDED
 # SEQUENCE    = DEFAULT 0
@@ -32,17 +32,31 @@ for i in range(packet_size):		# fill packet with sequential values
 a.set(packet)						# only on changes
 
 # ALL PACKETS ARE SAVED IN THE CLASS, YOU CAN CHANGE SINGLE VALUES
-a.set_single_value(1, 255)
+a.set_single_value(1, 255)			# set channel 1 to 255
 
 # ... AND SEND
-a.show()
+a.show()							# send data
 
 # OR USE STUPIDARTNET FUNCTIONS
-a.flash_all()
+a.flash_all()						# send single packet with all channels at 255
 
 time.sleep(1)						# wait a bit, 1 sec
 
-a.blackout()
+a.blackout()						# send single packet with all channels at 0
+
+# ALL THE ABOVE EXAMPLES SEND A SINGLE DATAPACKET
+# STUPIDARTNET IS ALSO THREADABLE
+# TO SEND PERSISTANT SIGNAL YOU CAN START THE THREAD
+a.start()														# start continuos sendin
+
+# AND MODIFY THE DATA AS YOU GO
+for x in range(100):
+	for i in range(packet_size):	# Fill buffer with random stuff
+		packet[i] = random.randint(0, 255)
+	a.set(packet)
+	time.sleep(.2)
+# ... REMEMBER TO CLOSE THE THREAD ONCE YOU ARE DONE
+a.stop()
 
 # CLOSE THE SOCKET IN THE END, JUST TO BE SURE
 a.close()
