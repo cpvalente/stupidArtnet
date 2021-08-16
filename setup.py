0a2359889d9ea2dcb062e051913db337d36cde63
Carlos Valente <34649812+cpvalente@users.noreply.github.com>
2021-08-16 23:07:29 +0200

    initial config

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup
+import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
-setup(name='stupid-artnet',
-      version='1.0',
-      license='MIT',
-      description='(Very) Simple Python library implementation of the Art-Net protocol',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-	  url="https://github.com/cpvalente/stupidArtnet",
-      packages=['lib'],
-      zip_safe=False)
+setuptools.setup(name='stupidArtnet',
+                 author='cpvalente',
+                 version='1.0',
+                 license='MIT',
+                 description='(Very) Simple implementation of the Art-Net protocol',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/cpvalente/stupidArtnet',
+                     project_urls={
+                         'Bug Tracker': 'https://github.com/cpvalente/stupidArtnet/issues',
+                     },
+                 classifiers=[
+                         'Programming Language :: Python :: 3',
+                         'License :: OSI Approved :: MIT License',
+                         'Operating System :: OS Independent',
+                     ],
+                 keywords=['LIGHTING', 'DMX', 'LIGHTING CONTROL'],
+                 packages=['stupidArtnet'],
+                 package_dir='stupidArtnet',
+                 packages=setuptools.find_packages(where='stupidArtnet'),
+                 python_requires=">=3.6",
+                 zip_safe=False)
