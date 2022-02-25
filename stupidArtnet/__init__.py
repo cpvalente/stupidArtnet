c8f1af8624df08ecaf6799419f50770e9d308587
cv <34649812+cpvalente@users.noreply.github.com>
2022-02-25 12:50:11 +0100

    refact: missing docstring

--- stupidArtnet/__init__.py
+++ stupidArtnet/__init__.py
@@ -1,3 +1,7 @@
+"""
+Facilitates library imports
+"""
+
 from stupidArtnet.StupidArtnetServer import StupidArtnetServer
 from stupidArtnet.ArtnetUtils import shift_this, put_in_range, make_address_mask
 from .StupidArtnet import StupidArtnet
