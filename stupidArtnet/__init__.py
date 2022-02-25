11f8025ceb73f5962c89a9133d3a2aba4e502670
cv <34649812+cpvalente@users.noreply.github.com>
2022-02-25 12:45:22 +0100

    refact: import order

--- stupidArtnet/__init__.py
+++ stupidArtnet/__init__.py
@@ -1,3 +1,3 @@
-from .StupidArtnet import StupidArtnet
 from stupidArtnet.StupidArtnetServer import StupidArtnetServer
 from stupidArtnet.ArtnetUtils import shift_this, put_in_range, make_address_mask
+from .StupidArtnet import StupidArtnet
