3033241b47360348fc701405807ee4e1c4bd12b1
Luca Sorace "Stranck <strdjn@gmail.com>
2023-04-27 15:32:11 +0200

    Bump versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(name='stupidArtnet',
       author='cpvalente',
-      version='1.3.0',
+      version='1.4.0',
       license='MIT',
       description='(Very) Simple implementation of the Art-Net protocol',
       long_description=long_description,
